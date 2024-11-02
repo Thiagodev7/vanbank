e8949cdf103157e21a94dff3e2d3ddaddf94209c
Thiago <email@thiagols.com.br>
2024-11-02 14:55:52 -0300

    animações

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,8 +6,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Variável para controlar a posição do Container
-  double _containerOffset = 0.0;
+  double _containerOffset = 300.0; // Começa fora da tela (abaixo)
+  bool _showMessage = false; // Controla a exibição da mensagem
+  bool _showButtons = false; // Controla a exibição dos botões
 
   @override
   void initState() {
@@ -15,7 +16,19 @@ class _HomeState extends State<Home> {
     // Inicia a animação após um pequeno atraso
     Future.delayed(Duration(milliseconds: 100), () {
       setState(() {
-        _containerOffset = 100.0; // Altera a posição para 0 quando inicia
+        _containerOffset = 0.0; // Muda a posição para 0 para aparecer na tela
+      });
+
+      // Espera a animação do container terminar para mostrar a mensagem e os botões
+      Future.delayed(Duration(milliseconds: 1000), () {
+        setState(() {
+          _showMessage = true; // Mostra a mensagem após a animação
+        });
+        // Future.delayed(Duration(milliseconds: 1000), () {
+        //   setState(() {
+        //     _showButtons = true; // Mostra os botões após um pequeno atraso
+        //   });
+        // });
       });
     });
   }
@@ -23,46 +36,138 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 100), // Espaço acima da logo
-                Image.asset('assets/logo.png', width: 300), // Logo
-                const SizedBox(height: 80), // Espaço entre a logo e o texto
-                const SizedBox(height: 20), // Espaço entre o nome e o container
-              ],
-            ),
-            AnimatedPositioned(
-              duration: const Duration(milliseconds: 1000),
-              curve: Curves.easeInOut,
-              bottom: _containerOffset, // Anima a posição do container
-              child: Container(
-                width: MediaQuery.of(context).size.width *
-                    1, // Ocupa toda a largura
-                decoration: const BoxDecoration(
-                  color: Colors.orange, // Cor de fundo
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(90), // Bordas arredondadas em cima
-                  ),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Olá, seja bem-vindo!',
-                    style: TextStyle(
-                      color: Colors.white, // Cor do texto
-                      fontSize: 18,
-                    ),
-                    textAlign: TextAlign.center, // Centraliza o texto
-                  ),
+      body: Column(
+        children: [
+          const SizedBox(height: 100), // Espaço acima da logo
+          AnimatedOpacity(
+            opacity: _showMessage ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+            child: Image.asset('assets/logo.png', width: 400), // Logo
+          ),
+          const SizedBox(height: 30), // Espaço entre a logo e o texto
+          AnimatedOpacity(
+            opacity: _showMessage ? 1.0 : 0.0,
+            duration: const Duration(milliseconds: 500),
+            child: Center(
+              child: Text(
+                'VANBANK',
+                style: TextStyle(
+                  color: Color(0xFFFFC7D45), // Cor do texto
+                  fontSize: 80, // Tamanho do texto
+                  fontWeight: FontWeight.bold, // Deixa o texto em negrito
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 1000),
+              curve: Curves.easeInOut,
+              transform: Matrix4.translationValues(0, _containerOffset, 0),
+              height: MediaQuery.of(context).size.height * 0.5, // 50% da altura
+              width: MediaQuery.of(context).size.width, // Ocupa toda a largura
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFC7D45), // Cor de fundo
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(90), // Bordas arredondadas em cima
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                      child: AnimatedOpacity(
+                          opacity: _showMessage ? 1.0 : 0.0,
+                          duration: const Duration(milliseconds: 1000),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              // Linha à esquerda do texto
+                              Container(
+                                width: 4, // Largura da linha
+                                height: 110, // Altura da linha
+                                color: Colors.white, // Cor da linha
+                              ),
+                              const SizedBox(
+                                  width: 10), // Espaço entre a linha e o texto
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Olá,',
+                                    style: TextStyle(
+                                      color: Colors.white, // Cor do texto
+                                      fontSize: 45, // Tamanho do texto
+                                    ),
+                                  ),
+                                  Text(
+                                    'seja bem-vindo!',
+                                    style: TextStyle(
+                                      color: Colors.white, // Cor do texto
+                                      fontSize: 45, // Tamanho do texto
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )) // Não exibe nada antes
+                      ),
+                  const SizedBox(
+                      height: 40), // Espaço entre o texto e os botões
+                  // Exibe os botões apenas se _showButtons for true
+
+                  AnimatedOpacity(
+                    opacity: _showMessage ? 1.0 : 0.0,
+                    duration: const Duration(milliseconds: 1000),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação para "Quero abrir uma conta"
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Color(0xFFFFC7D45),
+                            backgroundColor: Colors.white, // Cor do botão
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10), // Tamanho do botão
+                          ),
+                          child: const Text(
+                            'Quero abrir uma conta',
+                            style: TextStyle(
+                              fontSize: 30, // Aumente o tamanho do texto
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação para "Login"
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Color(0xFFFFC7D45),
+                            backgroundColor: Colors.white, // Cor do botão
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 10), // Tamanho do botão
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 30, // Aumente o tamanho do texto
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
